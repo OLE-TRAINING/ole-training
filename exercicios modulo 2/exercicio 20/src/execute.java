c3ff187157d201493b42327718771e36e8e95c28
Your Name <ferraresibruno@hotmail.com>
2018-08-24 15:04:57 -0300

    exercicio 20 identado e mais eficiente um pouco

--- exercicios modulo 2/exercicio 20/src/execute.java
+++ exercicios modulo 2/exercicio 20/src/execute.java
@@ -1,106 +1,87 @@
 import java.util.Scanner;
-public class execute {
+
+public class Execute {
 
 	public static void main(String[] args) {
-		  Scanner s= new Scanner(System.in);
-		  System.out.println("digite a altura da forma:");
-		  int n = s.nextInt();
-		  
-		  
-		  
-		  for(int i=0;i<n;i++){
-			  for(int j=0;j<=i;j++){
-					 System.out.print("*");
-			  }
-			  System.out.println();
-		  }
-		  
-		  
-		  
-		  for(int i=n;i!=0;i--){
-			  for(int j=0;j<i;j++){
-					 System.out.print("*");
-			  }
-			  System.out.println();
-		  } 
-		  
-		  
-		 
-		  for(int i=0;i<n;i++){
-			  
-			  for(int j=0;j<n;j++){
-					 if(j>i)
-						 System.out.print(" ");
-				  }
-			  
-			   for(int j=0;j<n;j++){
-					 if(j<=i)
-						 System.out.print("*");
-				  } 
-			   
-			 
-			  
-			  /*
---------------------------------------------------------------------------------------------
-			   * precisa printar todos espaços para começar a printar os *
-		        se os dois for do aninhamento interno for substituido por:
-		        
-		        for(int j=0;j<n;j++){
-					 if(j>i){
-						 System.out.print(" ");
-					 }else{
-						 System.out.print("*");
-					 }
-				  }
-				  
-			para uma entrada com o valor 10 a forma seguinte é printada
-*         
-**        
-***       
-****      
-*****     
-******    
-*******   
-********  
-********* 
-**********
-            ao invez da forma correta:
-		       
-		 *
-        **
-       ***
-      ****
-     *****
-    ******
-   *******
-  ********
- *********
-**********
-   
-   	      nao encherguei por hora uma forma de substituir esses 2 for do aninhamento interno
---------------------------------------------------------------------------------------------	            
-		      */
-			  
-			   
-			   
-			  System.out.println();
-		  } 
-	
-		  
-		  
-		  for(int i=0;i<n;i++){
-			  for(int j=n;j!=0;j--){
-					 if(j<=i)
-						 System.out.print(" ");
-				  }
-			  for(int j=n;j!=0;j--){
-				 if(j>i)
-					 System.out.print("*");
-			  }
-			  System.out.println();
-		  } 
-		  
-		  
+		Scanner s = new Scanner(System.in);
+		System.out.println("digite a altura da forma:");
+		int height = s.nextInt();
+
+		writeForm1(height);
+
+		writeForm2(height);
+
+		writeForm3(height);
+
+		writeForm4(height);
 	}
 
+	public static void writeForm1(int h) {
+
+		System.out.println();
+
+		for (int row = 0; row < h + 1; row++) {
+
+			writeRowType1(row);
+
+			System.out.println();
+		}
+	}
+
+	public static void writeForm2(int height) {
+
+		for (int row = height; row != 0; row--) {
+
+			writeRowType1(row);
+
+			System.out.println();
+		}
+	}
+
+	public static void writeRowType1(int row) {
+		for (int column = 0; column < row; column++) {
+			System.out.print("*");
+		}
+	}
+
+	public static void writeForm3(int height) {
+
+		System.out.println();
+
+		for (int row = 0; row < height; row++) {
+
+			writeRowType3(row, height);
+
+			System.out.println();
+		}
+	}
+
+	public static void writeRowType3(int row, int height) {
+		for (int column = height - 1; column >= 0; column--) {
+			if (column > row) {
+				System.out.print(" ");
+			} else {
+				System.out.print("*");
+			}
+		}
+	}
+
+	public static void writeForm4(int height) {
+
+		for (int row = 0; row < height; row++) {
+			writeRowType4(row, height);
+
+			System.out.println();
+		}
+	}
+
+	public static void writeRowType4(int row, int height) {
+		for (int column = 0; column < height; column++) {
+			if (column >= row) {
+				System.out.print("*");
+			} else {
+				System.out.print(" ");
+			}
+		}
+	}
 }
