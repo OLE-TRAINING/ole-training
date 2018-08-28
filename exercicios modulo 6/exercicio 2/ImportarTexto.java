53c5f8cc00f18616bcdf247e474643992849d012
Your Name <ferraresibruno@hotmail.com>
2018-08-28 14:19:06 -0300

    exercicios modulo 6

---
+++ exercicios modulo 6/exercicio 2/ImportarTexto.java
@@ -0,0 +1,17 @@
+package ImportarTexto;
+public class ImportarTexto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void importarArquivo() {
+		
+	}
+	
+	public void filtrarDados() {
+		
+	}
+
+}
---
+++ exercicios modulo 6/exercicio 6/ImportarTexto/ImportarTexto.java
@@ -0,0 +1,18 @@
+package ImportarTexto;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class ImportarTexto {
+
+	public static void main(String[] args) {  
+	  
+	    JFrame frame = new JFrame("JOptionPane showMessageDialog example");
+	    
+	   
+	    JOptionPane.showMessageDialog(frame,"Teste de arquivo JAR");
+	    System.exit(0);
+	}
+
+}
